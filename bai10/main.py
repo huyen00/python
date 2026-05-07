a3472eac74328c9c57bfb5cc7c6bf056a76ad617
huyenht <hoanghuyenn2k@gmail.com>
2026-05-08 06:59:36 +0700

    bài 10 thư viện module python

---
+++ bai10/main.py
@@ -0,0 +1,176 @@
+# 10.Brief tour of the standard library (thư viện có sẵn trong python)
+# 10.1: Operating system interface (Giao diện hệ điều hành os)
+# ---- module
+import os
+print(os.getcwd()) #in ra thư mục làm việc hiện tại của project
+os.chdir('/python/python') #thay đổi thư mục làm việc hiện tại
+# os.system('mkdir tody1') # tạo 1 thư mục mới
+print("thư mục làm việc hiện tại: ", os.getcwd())
+print(os.listdir()) # trả về danh sách các file và thư mục trong thư mục hiện tại
+# print("các hàm trong module os là:", dir(os)) # trả về hàm trong ds của module os
+# os.mkdir("huyen") # tạo thư mục mới có tên huyen
+# os.rmdir("Thư mục được tạo mới") # xóa thư mục 
+# ---------
+import shutil # cung cấp interface cấp cao hơn để quản lý file/thư mục dễ hơn.
+shutil.copyfile('D:/python/python/bai10/main.py', 'D:/python/python/bai10/text.txt')
+shutil.move('D:/python/python/bai10/text.txt', 'D:/python/python/bai10/new_text.txt') # di chuyển file sang 1 thư mục/file khác
+
+# shutil.rmtree("tody1") # xóa thư mục tody1 cùng tất cả nội dung bên trong
+# ==============================
+# 10.2: File wildcards (module glob: dùng để tìm file, lọc file theo pattern)
+# Ký tự	    Ý nghĩa
+# *	        mọi ký tự
+# ?	        1 ký tự
+# []	    nhóm ký tự
+
+import glob
+
+f= glob.glob('*.py')
+print("các file có đuổi .b là:", f) #list các file có đuổi .py trong thư mục làm việc hiện tại/ sử dụng os.chdir để thay đổi thư mục làm việc và lọc tìm file
+y = glob.glob('READM?.md') # tìm đúng 1 kí tự cuối cùng có tên file trùng với
+print("file có tên trùng với bai?.py",y)
+
+Z = glob.glob("bai[45].py") # tìm file có bắt đầu là "bai", [45]: chỉ được là 4 hoặc 5, kết thúc bằng .py
+print("tìm file có []:", Z)
+# ==================================
+# 10.3: Command-line arguments ( tham số dòng lệnh)
+#  Phần này nói về cách python nhận dữ liệu khi chạy chương trình từ terminal/cmd
+# --- sys.argv
+import sys
+sys.argv
+print("sys.argv:", sys.argv) #['bai10/main.py','one','two','three']: argv[0]: tên file python, argv[1]:one, argv[2]:two, argv[3]:three
+# a = int(sys.argv[1])
+# b = int(sys.argv[2])
+# print("a+b:",a+b) # 13
+# -------- argparse(module dùng để parse arguments/ validate kiểu dữ liệu/ tạo help command)
+import argparse
+# parser = argparse.ArgumentParser(prog ='main', description='shơ tai liệu') # tạo đổi tượng parser để đọc tham số command line
+# parser.add_argument("filenames", nargs = '+') # phải có ít nhất 1 giá trị và có thể nhièu hơn 1
+# parser.add_argument('-l','--lines', type =int, default=10)
+# args = parser.parse_args() #phân tích command line và trả về object kiểu NameSpace:args = Namespace( filenames=['a.txt', 'b.txt'],lines=10)
+# print("args:",args) # câu lệnh chạy: python bai10/main.py -l 20 a.txt b.txt
+# print("args.file", args.filenames)
+# print("args.line", args.lines)
+
+# ==============================
+
+# 10.4: Error output redirection and program termination (Chuyển hướng output lỗi và kết thúc chương trình)
+# module sys cũng có các thuộc tính: stdin/stdout/stderr
+# + stderr: hiển thị warning/hiển thị error message kể cả khi stdout đã bị redirect
+#  + stdin: dữu liệu nhập vào
+#  + stdout: output bình thường
+#  + stderr: output lỗi/ cảnh báo
+#  + sys.exit():kết thúc chương trình
+
+sys.stdout.write("Hello\n")
+sys.stderr.write("warning, log file not found starting a new one")
+
+print("Đây là output thường")
+sys.stderr.write("Đây là warning\n")
+
+# =====================================
+
+# 10.5:String pattern matching (xử lý mẫu chuỗi: module re): cung cấp các công cụ Regular Expression để xử lý chuỗi nâng cao
+# tìm kiếm/thay thế/kiểm tra pattern
+import re
+r= re.findall(r'\bo[a-z]*', 'which foot or hand fell fastest')
+print("Tifm tất cả f:", r)
+# re.findall(): trả về tất cả phần match
+# 🧠 Phân tích regex
+# Regex	    Ý nghĩa
+# \b	    bắt đầu từ đầu từ
+# f	        chữ f
+# [a-z]	    ký tự a-z
+# *	        0 hoặc nhiều lần
+# (\b[a-z]+)    lấy 1 từ
+# \1            lặp lại từ trước đó
+
+# ----------- re.sub(): dùng để thay thế bằng regex
+sub = re.sub(r'(\b[a-z]+) \1', r'\1', 'cat in the the hat') # từ the the --> the
+print("thay thế:", sub)
+
+# regex ---------- ý nghĩa
+# .         mọi kí tự
+#  *        0+ lần
+#  +        1+ lần
+# ?         0 hoặc 1
+#  [a-z]    kí tự
+#  \d       số
+# \w        chữ + số
+# \s        khoảng trắng
+# ^         bắt đầu
+# $         kết thúc
+
+# ----- module re
+# findall: tìm tất cả kết quả match
+# search:tìm kiếm match đầu tiên
+# match: kiểm tra pattern ở đầu chuỗi
+# sub : thay thế
+
+# =============================
+
+# 10.6: Mathematics (module toán học)
+import math #cung cấp các hàm toán học
+
+# Hàm	    Ý nghĩa
+# sqrt()	căn bậc hai
+# pow()	    lũy thừa
+# sin()	    sin
+# cos()	    cos
+# log()	    log
+# ceil()	làm tròn lên
+# floor()	làm tròn xuống
+
+import random #tạo dữ liệu ngẫu nhiên
+
+# Hàm           Ý nghĩa
+# choice         chọn 1 phần tử
+# sample()       chọn nhiều phần tử
+# random()       số thực random
+# randint(a,b)   Số nguyên
+# shuffle()      trộn list
+
+import statistics
+# Hàm           ý nghĩa
+# mean()        trung bình
+# median()      trung vị
+# mode()        giá trị xuất hiện nhiều
+# variance()    phương sai
+# stdev()       độ lệch chuẩn
+
+# ====================================
+
+# 10.7: internet access(truy cập internet)
+# lấy dữ liệu từ internet
+# gửi email
+# làm việc với giao thức mạng
+
+# -----------
+
+# + urllib.request (dùng truy cập url/ tải dữ liệu từ web)
+from urllib.request import urlopen
+with urlopen('https://docs.python.org/3/') as respon:
+    for line in respon:
+        line = line.decode()
+        if 'updated' in line:
+            print("line.rstrip()", line.rstrip())
+print("respon", respon)
+#  + smtplib : dùng để gửi mail 
+import smtplib
+server = smtplib.SMTP('localhost')
+server.sendmail(
+    'soothsayer@example.org',
+    'jcaesar@example.org',
+    """To: jcaesar@example.org
+From: soothsayer@example.org
+
+Beware the Ides of March.
+"""
+)
+server.quit() # đóng kết nối email
+
+# 🧠 Ý nghĩa
+# Tham số	Ý nghĩa
+# sender	email gửi
+# receiver	email nhận
+# message	nội dung mail
